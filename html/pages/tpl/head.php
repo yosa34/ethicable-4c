0d24fee69aea9f4aa184c4e39c2dc53518873485
yosa34 <osaru.san.0234@gmail.com>
2020-06-15 12:54:41 +0900

    ログイン認証をheadのテンプレートへ移動

--- html/pages/tpl/head.php
+++ html/pages/tpl/head.php
@@ -41,6 +41,43 @@
   <!-- firebase設定js -->
   <script type="text/javascript" src="./js/firebase.js"></script>
 
+  <!-- ログイン認証 -->
+  <script>
+  const db = firebase.firestore();
+
+  //ログアウト処理
+  function logout(){
+    firebase.auth().signOut();
+  }
+
+  firebase.auth().onAuthStateChanged(function(user) {
+        //ログイン状態判別
+        if (user) {
+          console.log(user.uid);
+          // firebase データ読み込み
+          let citiesRef = db.collection('user').where("user_id", "==", user.uid);
+          let allCities = citiesRef.get().then(snapshot => {
+              snapshot.forEach(doc => {
+                const data = doc.data()
+                console.log(data);
+                // var userData = [];
+                // this.userData.push({
+                //   key:doc.id,
+                //   });
+                // console.log(this.userData);
+              });
+            })
+            .catch(err => {
+              console.log('Error getting documents', err);
+            });
+        } else{
+          location.href = "./index.html"
+        }
+    });   
+
+
+</script>
+
 
 
 
