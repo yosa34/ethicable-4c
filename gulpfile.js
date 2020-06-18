c10326742196a6d223f4543ab15737e22b33d29d
yosa34 <osaru.san.0234@gmail.com>
2020-06-18 11:43:34 +0900

    sassコンパイル設定編集

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var sass = require('gulp-sass');
 */
 gulp.task('sass', function(done){
     // stream
-    gulp.src('./html/pages/style/sass/*.scss') //タスクで処理するソースの指定
+    gulp.src('./html/pages/style/sass/**/*.scss') //タスクで処理するソースの指定
     .pipe(sass()) //処理させるモジュールを指定
     .pipe(gulp.dest('./html/pages/style/css/')); //保存先を指定
 
@@ -24,7 +24,7 @@ gulp.task('sass', function(done){
     コマンド：gulp　watch
 */
 gulp.task('watch', function(done){
-    gulp.watch('./html/pages/style/sass/*.scss', gulp.task('sass'));
+    gulp.watch('./html/pages/style/sass/**/*.scss', gulp.task('sass'));
     //watch task
     console.log('watch start');
     done();
