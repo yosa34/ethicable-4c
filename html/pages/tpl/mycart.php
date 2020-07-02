cd37f566727d03f47d54826e3312100c33d5d22a
yosa34 <osaru.san.0234@gmail.com>
2020-07-02 17:36:58 +0900

    カート画面コーディング完了

--- html/pages/tpl/mycart.php
+++ html/pages/tpl/mycart.php
@@ -26,6 +26,40 @@
 
     <!-- main -->
     <main>
+      <h1>カート</h1>
         <section>
+          <ul>
+              <li>
+                  <p><img src="./image/product/414443.jpg" alt=""></p>
+                  <div>
+                      <dl>
+                          <dt>部門(カテゴリー)：</dt>
+                          <dd><img src="./image/category/1.png" alt=""><p>アウター</p></dd>
+                      </dl>
+                      <dl>
+                          <dt>カラー：</dt>
+                          <dd><span></span><p>GREY</p></dd>
+                      </dl>
+                      <p>価格：4,500円</p>
+                      <p>削除</p>
+                  </div>
+              </li>
+          </ul>
+          <div>
+            <dl>
+              <dt>小計<span>(2商品)</span></dt>
+              <dd>9,000円</dd>
+            </dl>
+            <dl>
+              <dt>送料</dt>
+              <dd>1,500円</dd>
+            </dl>
+          </div>
+          <dl>
+            <dt>獲得予定</dt>
+            <dd>253ポイント(円相当)</dd>
+          </dl>
         </section>
+        <p>合計<b>10,500</b>円</p>
+        <input type="button" value="購入手続き">
     </main>
