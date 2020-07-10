2128936da62bf9574ea05a52a7b0f82ca100e86c
yosa34 <osaru.san.0234@gmail.com>
2020-07-11 02:04:02 +0900

    metaタグ追加

--- html/pages/tpl/head.php
+++ html/pages/tpl/head.php
@@ -5,13 +5,13 @@
   <meta http-equiv="content-language" content="ja">
   <meta charset="UTF-8"/>
   <!-- キーワード -->
-  <meta name="keywords" content="">
+  <meta name="keywords" content="ethicable,エシカブル,リメイク,SDGs,3R">
   <!-- 検索結果に表示される文字 -->
-  <meta name="description" content="">
+  <meta name="description" content="ethicable(エシカブル)は、お手持ちの不必要になった衣料を好みに応じてリメイクしたり回収してリメイクしリサイクルショップで販売することができます。">
   <!-- 検索エンジンクローラにURLをインデックスしないように(noindex)文書内のURLを辿らないように(nofollow)知らせるため -->
   <meta name="robots" content="noindex,nofollow">
   <!-- 文書の作者 -->
-  <meta name="author" content="ethical">
+  <meta name="author" content="ethicable">
 
   <!-- ファビコン -->
 <link rel="icon" href="../../favicon.ico">
