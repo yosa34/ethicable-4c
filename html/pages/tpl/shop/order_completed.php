ff01bc3123fb7fefa7ce568bf82c067e429d06d4
Miwa <miwa.kento.303@gmail.com>
2020-07-17 18:19:02 +0900

    sessionのclearが効いてなかったので、効くようにしました。

--- html/pages/tpl/shop/order_completed.php
+++ html/pages/tpl/shop/order_completed.php
@@ -107,33 +107,11 @@
         // 購入処理が完了したら...
         purchase_promise.then((flg) => {
           // sessionを削除
-          // window.sessionStorage.clear();
-          console.log(flg);
-          window.location = './shop_complete.php';
+          window.sessionStorage.clear();
         });
 
         // sessionStorageのifここまで
       }
-
-      /**
-      <!-- header -->
-        <?php include "./tpl/header.html"; ?>
-
-        <!-- main -->
-        <main>
-        <h1>注文完了</h1>
-
-          <section>
-          <p>注文が完了しました</p>
-          <p>アカウントのメールアドレス宛に注文完了通知が送られます</p>
-          <p>ご確認ください</p>
-
-          <p><a href="shop_home.php">ショップへ</a></p>
-
-          </section>
-
-        </main>
-       */
     }
   });
 </script>
@@ -142,3 +120,21 @@
 
 </head>
   <body id="order_completed">
+
+    <!-- header -->
+    <?php include "./tpl/header.html"; ?>
+
+    <!-- main -->
+    <main>
+    <h1>注文完了</h1>
+
+      <section>
+      <p>注文が完了しました</p>
+      <p>アカウントのメールアドレス宛に注文完了通知が送られます</p>
+      <p>ご確認ください</p>
+
+      <p><a href="shop_home.php">ショップへ</a></p>
+
+      </section>
+
+    </main>
